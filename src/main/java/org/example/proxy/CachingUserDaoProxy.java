04cd7446f67be5043bb0ad791f0890dea4a0dd21
Ann_Mikhaliova <annmikhaleva35@gmail.com>
2023-11-19 19:23:13 +0300

    feat: create factory

---
+++ src/main/java/org/example/proxy/CachingUserDaoProxy.java
@@ -0,0 +1,83 @@
+package org.example.proxy;
+
+import lombok.RequiredArgsConstructor;
+import org.example.cache.Cache;
+import org.example.dao.UserDao;
+import org.example.entity.User;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CachingUserDaoProxy implements UserDao {
+
+    private final UserDao userDao;
+    private final Cache<User> cache = new Factory<User>().createCache();
+
+    /**
+     * Ищет в кэш пользователя по идентификатору
+     *
+     * @param id идентификатор пользователя
+     * @return возвращает пользователя найденого в кэш
+     */
+    @Override
+    public User findById(Long id) {
+        User user = cache.get(id);
+        if (user != null) {
+            return user;
+        }
+        user = userDao.findById(id);
+        cache.put(user.getId(), user);
+        return cache.get(id);
+    }
+
+    /**
+     * Ищет всех пользователей в БД
+     *
+     * @return список найденных пользователей
+     */
+    @Override
+    public List<User> findAll() { // как закидывать, если есть ограничения по кэш?
+        return userDao.findAll();
+    }
+
+    /**
+     * Сохраняет пользователя в кэш
+     *
+     * @param user сохраняемый пользователь
+     * @return возвращает пользователя сохраненного в кэш
+     */
+    @Override
+    public User create(User user) {
+        userDao.create(user);
+        cache.put(user.getId(), user);
+        return cache.get(user.getId());
+    }
+
+    /**
+     * Обновляет пользователя в кэш
+     *
+     * @param user сохраняемый пользователь
+     * @return возвращает пользователя обнавленного в кэш
+     */
+    @Override
+    public User update(User user) {
+        userDao.update(user);
+        cache.put(user.getId(), user);
+        return cache.get(user.getId());
+    }
+
+    /**
+     * Удаляет пользователя из кэша по идентификатору
+     *
+     * @param id идентификатор пользователя
+     */
+    @Override
+    public boolean delete(Long id) {
+        userDao.delete(id);
+        if (cache.get(id) != null) {
+            cache.delete(cache.get(id).getId());
+        }
+        return true;
+    }
+
+}
